8dad6ac0321992dfb29096d176c3f410f3f476da
akachiryo <rirugameri@icloud.com>
2022-01-18 01:37:10 +0900

    userプロフィール　introduction avatar 追加

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,12 +1,27 @@
 class Api::UsersController < ApplicationController
+
+  def show
+    user = User.find(params[:id])
+    render json: user, serializer: UserSerializer
+  end
+  
   def create
     user = User.new(user_params)
     user.save!
     render json: user, status: :created, serializer: UserSerializer
   end
 
+  def update
+    current_user.update(user_update_params)
+    render json: current_user, serializer: UserSerializer
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def user_update_params
+    params.require(:user).permit(:name, :introduction, :avatar)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_17_145214) do
+ActiveRecord::Schema.define(version: 2022_01_17_162944) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 2022_01_17_145214) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "admin", default: false, null: false
+    t.text "introduction"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
