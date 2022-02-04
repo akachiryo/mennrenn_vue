a0d460ffe6995215fe27d327d68df72072946150
akachiryo <rirugameri@icloud.com>
2022-02-04 13:06:01 +0900

    db:reset できるよう整理

--- db/migrate/20220128050943_remove_user_from_tags.rb
+++
@@ -1,6 +0,0 @@
-class RemoveUserFromTags < ActiveRecord::Migration[6.0]
-  def change
-    remove_foreign_key :tags, :users
-    remove_reference :tags, :users, index: true
-  end
-end
--- db/migrate/20220128052533_tags.rb
+++
@@ -1,5 +0,0 @@
-class Tags < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :tags
-  end
-end
